458cb6101e2fb7d620f95a1ced1da883cab236f3
Sander <hey@sandydoo.me>
2023-05-15 14:58:46 +0000

    Add an example Warp server

---
+++ example/Main.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Function                                 ((&))
+import qualified Network.Wai.Handler.Warp         as Warp
+import qualified Network.Wai.Handler.Warp.Systemd as Systemd
+import Network.Wai
+import Network.HTTP.Types.Status                     (status200)
+
+app :: Application
+app _ respond =
+  respond $ responseLBS status200 [] "Hello World"
+
+main :: IO ()
+main =
+  Systemd.runSystemdWarp systemdSettings Warp.defaultSettings app
+  where
+    systemdSettings = Systemd.defaultSystemdSettings
+      & Systemd.setHeartbeatInterval (Just 5)
